59e62233964d069c75a3d1dad79ecfcfdf24f597
Frank Wallace <faw987@yahoo.com>
2013-01-10 20:14:13 -0500

    v 1.041 repurposed vendactivity (which was not being used) to call http

--- p1/src/com/example/p1/Utilities.java
+++ p1/src/com/example/p1/Utilities.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -19,36 +24,35 @@ public class Utilities {
 
 		return input;
 	}
-	
-	
-	public static String jsonToStringFromAssetFolder(int id,Context context) throws IOException {
-       // AssetManager manager = context.getAssets();
-      //  InputStream file = manager.open(fileName);
-        InputStream file = context.getResources().openRawResource(id);
 
-        byte[] data = new byte[file.available()];
-        file.read(data);
-        file.close();
-        return new String(data);
-    }
+	public static String jsonToStringFromAssetFolder(int id, Context context)
+			throws IOException {
+		// AssetManager manager = context.getAssets();
+		// InputStream file = manager.open(fileName);
+		InputStream file = context.getResources().openRawResource(id);
 
-	
+		byte[] data = new byte[file.available()];
+		file.read(data);
+		file.close();
+		return new String(data);
+	}
+
 	static public void getSamplePlans(Context myContext) throws IOException {
-	    AssetManager mngr = myContext.getResources().getAssets();
-	    InputStream is = mngr.open("/res/raw/plans.txt");
-	    InputStreamReader isr = new InputStreamReader(is);
-	    BufferedReader br = new BufferedReader(isr);
-	    String read = br.readLine();
+		AssetManager mngr = myContext.getResources().getAssets();
+		InputStream is = mngr.open("/res/raw/plans.txt");
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String read = br.readLine();
 
-	    while(read != null) {
-	        System.out.println(read);
-	        //sb.append(read);
-	        read =br.readLine();
+		while (read != null) {
+			System.out.println(read);
+			// sb.append(read);
+			read = br.readLine();
 
-	    }
+		}
 
 	}
-	
+
 	static public String getArrayPlanStrings(Context ac, String input) {
 
 		String result = "";
@@ -87,22 +91,24 @@ public class Utilities {
 	}
 
 	static public void readPlansTasks(Context c) {
-		String sampleplans=null, sampletasks=null;
+		String sampleplans = null, sampletasks = null;
 		try {
-			 sampleplans = Utilities.jsonToStringFromAssetFolder(R.raw.plans, c);
-			System.out.println("PlanActivity -- plans sampleplans=" + sampleplans);
-			 sampletasks = Utilities.jsonToStringFromAssetFolder(R.raw.tasks, c);
-			System.out.println("PlanActivity -- tasks sampletasks=" + sampletasks);
+			sampleplans = Utilities.jsonToStringFromAssetFolder(R.raw.plans, c);
+			System.out.println("PlanActivity -- plans sampleplans="
+					+ sampleplans);
+			sampletasks = Utilities.jsonToStringFromAssetFolder(R.raw.tasks, c);
+			System.out.println("PlanActivity -- tasks sampletasks="
+					+ sampletasks);
 
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("PlanActivity -- plans e=" + e);
-		};
+		}
+		;
 
 		JSONObject jsonObj = null;
 
 		// plans follow
-		
+
 		try {
 			jsonObj = new JSONObject(sampleplans);
 
@@ -127,33 +133,102 @@ public class Utilities {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		// tasks follow
-		
-				try {
-					jsonObj = new JSONObject(sampletasks);
 
-					JSONArray tasks = jsonObj.getJSONArray("tasks");
-					for (int i = 0; i < tasks.length(); i++) {
-						// printing the values to the logcat
-						System.out.println("ShowSettingsActivity: i:" + i);
+		try {
+			jsonObj = new JSONObject(sampletasks);
 
-						System.out.println("ShowSettingsActivity: name:"
-								+ tasks.getJSONObject(i).getString("name").toString());
-						System.out.println("ShowSettingsActivity: name:"
-								+ tasks.getJSONObject(i).getString("desc").toString());
+			JSONArray tasks = jsonObj.getJSONArray("tasks");
+			for (int i = 0; i < tasks.length(); i++) {
+				// printing the values to the logcat
+				System.out.println("ShowSettingsActivity: i:" + i);
 
-						Task t = new Task();
-						t.name = tasks.getJSONObject(i).getString("name").toString();
-						t.desc = tasks.getJSONObject(i).getString("desc").toString();
-						Globals g = Globals.getInstance();
-						g.addTask(t);
+				System.out.println("ShowSettingsActivity: name:"
+						+ tasks.getJSONObject(i).getString("name").toString());
+				System.out.println("ShowSettingsActivity: name:"
+						+ tasks.getJSONObject(i).getString("desc").toString());
 
-					}
+				Task t = new Task();
+				t.name = tasks.getJSONObject(i).getString("name").toString();
+				t.desc = tasks.getJSONObject(i).getString("desc").toString();
+				Globals g = Globals.getInstance();
+				g.addTask(t);
 
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static public String callHttp(String url ) {
+
+		HttpClient httpclient = new DefaultHttpClient();
+
+
+		System.out.println(" == postParam:" + url);
+
+//		HttpPost httppost = new HttpPost(postParam);
+		HttpGet httppost = new HttpGet(url);
+
+	//	httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+
+		try {
+
+//			String inputString = "<getIn><input1>" + input
+//					+ "</input1></getIn>";
+//
+//			StringEntity myEntity = new StringEntity(inputString, "UTF-8");
+//
+//			System.out.println("===============================");
+//			System.out.println(myEntity.getContentType());
+//			System.out.println(myEntity.getContentLength());
+//			System.out.println("inputString:" + inputString);
+//			System.out.println("myEntity:" + myEntity);
+//
+//			httppost.setEntity(myEntity);
+
+			HttpResponse response;
+
+			response = httpclient.execute(httppost);
+
+			System.out.println(" == httppost:" + httppost);
+			System.out.println(" == response:" + response);
+
+			BufferedReader in = null;
+
+			in = new BufferedReader(new InputStreamReader(response.getEntity()
+					.getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			String NL = System.getProperty("line.separator");
+			while ((line = in.readLine()) != null) {
+				sb.append(line + NL);
+			}
+			in.close();
+			String result = sb.toString();
+
+			System.out.println(" == result:" + result);
+
+			return result;
+
+		} catch (ClientProtocolException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		}
+
+		return null;
+
 	}
 
 }
--- p1/src/com/example/p1/VendActivity.java
+++ p1/src/com/example/p1/VendActivity.java
@@ -1,7 +1,10 @@
 package com.example.p1;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 
 public class VendActivity extends Activity {
@@ -15,10 +18,23 @@ public class VendActivity extends Activity {
 
 		System.out.println("      VendAct");
 		setContentView(R.layout.activity_vend);
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+				.permitAll().build();
+
+		StrictMode.setThreadPolicy(policy);
+		String r = Utilities.callHttp("https://api.twitter.com/1/users/show.json?screen_name=faw987&include_entities=true");
+		System.out.println("      VendAct  r=" + r);
+		JSONObject jsonObj = null;
+		try {
+			jsonObj = new JSONObject(r);
+
+			String id = jsonObj.getString("id");
+			System.out.println("VendAct: id:" + id);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
-
-	// ?? Log.i(TAG, "===== ENTER =====");
-
 
 }
